23839dd775820bc8d051935928a35d0677396e27
Nick Cipollo <njc1115@gmail.com>
2023-10-20 17:08:49 -0400

    Add initial support for alpaca

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use apca::{ApiInfo, Client};
+use apca::api::v2::account;
+
+#[tokio::main]
+async fn main() {
+    let api_info = ApiInfo::from_env().expect("failed to load alpaca info from env");
+    let client = Client::new(api_info);
+    let account = client.issue::<account::Get>(&()).await.unwrap();
+    let power = account.buying_power;
+    println!("Hello buying power: {power}");
 }
