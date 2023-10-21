caee16ff89643ad494c163fc4e5e348b8c2b35ae
Nick Cipollo <njc1115@gmail.com>
2023-10-21 14:45:12 -0400

    test alpaca api

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use apca::{ApiInfo, Client};
+use apca::{ApiInfo, Client, RequestError};
 use apca::api::v2::account;
+use apca::data::v2::last_quotes::LastQuotesReqInit;
+use apca::data::v2::{last_quotes, quotes};
+use apca::data::v2::quotes::{QuotesReq, QuotesReqInit};
+use chrono::{DateTime, Duration, Utc};
 
 #[tokio::main]
 async fn main() {
@@ -8,4 +12,34 @@ async fn main() {
     let account = client.issue::<account::Get>(&()).await.unwrap();
     let power = account.buying_power;
     println!("Hello buying power: {power}");
+
+    println!("VTI Historic Quotes: ---");
+    let end = Utc::now() - Duration::minutes(15);
+    let start = end - Duration::hours(2);
+    let quote_req = QuotesReqInit { ..Default::default() }
+        .init("VTI", start, end);
+    let quote_result = client.issue::<quotes::Get>(&quote_req).await;
+    if quote_result.is_err() {
+        let err = quote_result.unwrap_err();
+        println!("Endpoint err - {err}");
+        if let RequestError::Endpoint(get_err) = err {
+            println!("Get err - {get_err}")
+        }
+    } else {
+        let quote = quote_result.unwrap();
+        println!("Quote: {:?}", quote);
+    }
+
+    println!("Latest VTI: ---");
+    let latest_req = LastQuotesReqInit {
+        ..Default::default()
+    }.init(vec!["VTI", "SPY"]);
+
+    let latest = client.issue::<last_quotes::Get>(&latest_req).await.unwrap();
+    let quote = latest[0].clone().1;
+    let ask = quote.ask_price.to_f64().unwrap();
+    let bid = quote.bid_price.to_f64().unwrap();
+    println!("Spy and VTI: {:?}", latest);
+    println!("Ask and bid: {ask} , {bid}")
+
 }
