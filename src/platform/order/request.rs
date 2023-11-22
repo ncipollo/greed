9d1e674a3e09df07b991199ad6994adcd3b3545a
Nick Cipollo <njc1115@gmail.com>
2023-11-22 16:40:30 -0500

    Fixes #24 Add order request

---
+++ src/platform/order/request.rs
@@ -0,0 +1,16 @@
+use num_decimal::Num;
+use crate::platform::order::class::OrderClass;
+use crate::platform::order::order_type::OrderType;
+use crate::platform::order::time_in_force::TimeInForce;
+
+#[derive(Clone, Debug, Default, Eq, PartialEq)]
+struct OrderRequest {
+    pub class: OrderClass,
+    pub order_type: OrderType,
+    pub time_in_force: TimeInForce,
+    pub limit_price: Option<Num>,
+    pub stop_price: Option<Num>,
+    pub trail_price: Option<Num>,
+    pub trail_percent: Option<Num>,
+    pub extended_hours: bool
+}
