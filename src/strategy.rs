2913ac7ad4860451bec0e8b0fd7d90014fae756a
Nick Cipollo <njc1115@gmail.com>
2023-11-24 16:55:39 -0500

    Test buy

--- src/strategy.rs
+++ src/strategy.rs
@@ -1,14 +1,17 @@
-use std::sync::Arc;
 use crate::asset::AssetSymbol;
 use crate::config::strategy::StrategyConfig;
 use crate::error::GreedError;
 use crate::platform::account::Account;
+use crate::platform::order::amount::Amount;
 use crate::platform::order::Order;
 use crate::platform::position::Position;
 use crate::platform::quote::Quote;
+use crate::platform::request::OrderRequest;
 use crate::platform::FinancialPlatform;
 use itertools::Itertools;
 use log::info;
+use num_decimal::Num;
+use std::sync::Arc;
 
 mod symbols;
 
@@ -19,13 +22,11 @@ pub struct StrategyRunner {
 
 impl StrategyRunner {
     pub fn new(config: StrategyConfig, platform: Arc<dyn FinancialPlatform>) -> Self {
-        Self {
-            config,
-            platform,
-        }
+        Self { config, platform }
     }
     pub async fn run(&self) -> Result<(), GreedError> {
         info!("🧠 running strategy: {}", self.config.name);
+        // let _ = self.test_buy().await?;
         let _ = self.fetch_account().await?;
         let symbols = symbols::from_config(&self.config);
         let _ = self.fetch_quotes(symbols).await?;
@@ -33,6 +34,15 @@ impl StrategyRunner {
         let _ = self.fetch_open_orders().await?;
         info!("----------");
         Ok(())
+    }
+
+    async fn test_buy(&self) -> Result<Order, GreedError> {
+        info!("- test buy of VTI");
+        let request = OrderRequest::market_order_buy(
+            AssetSymbol::new("VTI"),
+            Amount::Notional(Num::from(10)),
+        );
+        self.platform.place_order(request).await
     }
 
     async fn fetch_account(&self) -> Result<Account, GreedError> {
@@ -62,7 +72,7 @@ impl StrategyRunner {
     }
 
     async fn fetch_open_orders(&self) -> Result<Vec<Order>, GreedError> {
-        info!("- fetching open positions");
+        info!("- fetching open orders");
         let orders = self.platform.open_orders().await?;
         for order in &orders {
             info!("-- {}", order);
