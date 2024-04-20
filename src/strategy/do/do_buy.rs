66170a45724cb2e798b7a022949392b9f386b091
Nick Cipollo <njc1115@gmail.com>
2024-04-20 14:28:55 -0400

    Add multiply to target asset

--- src/strategy/do/do_buy.rs
+++ src/strategy/do/do_buy.rs
@@ -1,1 +1,23 @@
-pub struct DoBuyRule;
+use crate::strategy::r#do::{DoResult, DoRule};
+use crate::strategy::state::StrategyState;
+use crate::strategy::when::WhenResult;
+
+pub struct DoBuyRule {
+    buy_percent: f64
+}
+
+impl DoBuyRule {
+    pub fn boxed(buy_percent: f64) -> Box<dyn DoRule> {
+        Box::new(Self { buy_percent })
+    }
+}
+
+impl DoRule for DoBuyRule {
+    fn evaluate(&self, state: &StrategyState, when_result: WhenResult) -> DoResult {
+        DoResult {
+            actions: vec![],
+            skipped: false,
+            skip_reason: Default::default(),
+        }
+    }
+}
