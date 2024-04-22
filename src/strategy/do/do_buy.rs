04aeeb5bd2b243a0e0106a98c1b79403a29fb570
Nick Cipollo <njc1115@gmail.com>
2024-04-22 08:53:19 -0400

    add percent method to num

--- src/strategy/do/do_buy.rs
+++ src/strategy/do/do_buy.rs
@@ -1,5 +1,6 @@
 use crate::strategy::r#do::{DoResult, DoRule};
 use crate::strategy::state::StrategyState;
+use crate::strategy::target::TargetAsset;
 use crate::strategy::when::WhenResult;
 
 pub struct DoBuyRule {
@@ -9,6 +10,13 @@ pub struct DoBuyRule {
 impl DoBuyRule {
     pub fn boxed(buy_percent: f64) -> Box<dyn DoRule> {
         Box::new(Self { buy_percent })
+    }
+
+    fn calculate_buy_amount(&self, state: &StrategyState, target_asset: &TargetAsset) -> f64 {
+        // Get position, get orders, and get account
+        // total desired amount = equity * target percent
+        // buy amount  = total desired position - postion - pending orders
+        0.0
     }
 }
 
