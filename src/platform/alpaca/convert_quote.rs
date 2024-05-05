a79bfffdec6cfa8e9c2eefa77253c01df293cc54
Nick Cipollo <njc1115@gmail.com>
2024-05-05 14:37:50 -0400

    Fixes #36 Use bars when quote is invalid

--- src/platform/alpaca/convert_quote.rs
+++ src/platform/alpaca/convert_quote.rs
@@ -1,6 +1,8 @@
 use apca::RequestError;
+
 use crate::asset::AssetSymbol;
 use crate::greed_error_from;
+use crate::platform::bars::Bars;
 use crate::platform::quote::Quote;
 
 impl From<(String, apca::data::v2::last_quotes::Quote)> for Quote {
@@ -16,4 +18,40 @@ impl From<(String, apca::data::v2::last_quotes::Quote)> for Quote {
     }
 }
 
+impl From<Bars> for Quote {
+    fn from(value: Bars) -> Self {
+        let symbol = value.symbol;
+        let bar = value.bars.first().cloned().unwrap_or_default();
+        Self {
+            time: bar.timestamp,
+            ask_price: bar.close.clone(),
+            ask_size: 0,
+            bid_price: bar.close.clone(),
+            bid_size: 0,
+            symbol,
+        }
+    }
+}
+
 greed_error_from!(RequestError<apca::data::v2::last_quotes::GetError>);
+
+#[cfg(test)]
+mod tests {
+    use num_decimal::Num;
+    use crate::date::DateTimeFixture;
+    use super::*;
+
+    #[test]
+    fn from_bars() {
+        let bars = Bars::fixture(AssetSymbol::new("AAPL"), 100);
+        let quote = Quote::from(bars);
+        let expected = Quote {
+            time: DateTimeFixture::utc(),
+            ask_price: Num::from(200),
+            bid_price: Num::from(200),
+            symbol: AssetSymbol::new("AAPL"),
+            ..Default::default()
+        };
+        assert_eq!(expected, quote);
+    }
+}
