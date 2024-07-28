08f1cbfaa8b0b4f8c8ef131f8bebefa5ca99967e
Nick Cipollo <njc1115@gmail.com>
2024-07-28 14:42:03 -0400

    Add simple strategy converter

--- src/config/simple/strategy.rs
+++ src/config/simple/strategy.rs
@@ -1,4 +1,9 @@
 use crate::asset::AssetSymbol;
+use crate::config::strategy::r#do::DoConfig;
+use crate::config::strategy::r#for::ForConfig;
+use crate::config::strategy::rule::RuleConfig;
+use crate::config::strategy::when::WhenConfig;
+use crate::config::strategy::StrategyConfig;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Default, Deserialize, PartialEq, Serialize)]
@@ -8,5 +13,105 @@ pub struct SimpleStrategyConfig {
     pub buy: Option<f64>,
     pub sell: Option<f64>,
     #[serde(default)]
-    pub skip: bool
+    pub skip: bool,
+}
+
+impl From<SimpleStrategyConfig> for StrategyConfig {
+    fn from(value: SimpleStrategyConfig) -> Self {
+        StrategyConfig {
+            name: value.asset.symbol.clone(),
+            buy: buy_rules(&value),
+            sell: sell_rules(&value),
+        }
+    }
+}
+
+fn buy_rules(simple_config: &SimpleStrategyConfig) -> RuleConfig {
+    if let Some(buy) = simple_config.buy {
+        RuleConfig {
+            for_config: ForConfig::Stock {
+                stock: simple_config.asset.clone(),
+            },
+            when_config: WhenConfig::BelowMedian {
+                below_median_percent: buy,
+                median_period: Default::default(),
+            },
+            do_config: DoConfig::Buy {
+                buy_percent: simple_config.amount,
+            },
+        }
+    } else {
+        RuleConfig::default()
+    }
+}
+
+fn sell_rules(simple_config: &SimpleStrategyConfig) -> RuleConfig {
+    if let Some(sell) = simple_config.sell {
+        RuleConfig {
+            for_config: ForConfig::Stock {
+                stock: simple_config.asset.clone(),
+            },
+            when_config: WhenConfig::GainAbove {
+                gain_above_percent: sell,
+            },
+            do_config: DoConfig::SellAll { sell_all: true },
+        }
+    } else {
+        RuleConfig::default()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn from_minimal_config() {
+        let simple_config = SimpleStrategyConfig {
+            asset: AssetSymbol::new("VTI"),
+            ..Default::default()
+        };
+        let expected = StrategyConfig {
+            name: "VTI".into(),
+            buy: RuleConfig::default(),
+            sell: RuleConfig::default(),
+        };
+        assert_eq!(expected, StrategyConfig::from(simple_config))
+    }
+
+    #[test]
+    fn from_full_config() {
+        let simple_config = SimpleStrategyConfig {
+            asset: AssetSymbol::new("VTI"),
+            amount: 0.5,
+            buy: Some(0.1),
+            sell: Some(0.2),
+            skip: true,
+        };
+        let expected = StrategyConfig {
+            name: "VTI".into(),
+            buy: RuleConfig {
+                for_config: ForConfig::Stock {
+                    stock: "VTI".into(),
+                },
+                when_config: WhenConfig::BelowMedian {
+                    below_median_percent: 0.1,
+                    median_period: Default::default(),
+                },
+                do_config: DoConfig::Buy {
+                    buy_percent: 0.5,
+                },
+            },
+            sell: RuleConfig {
+                for_config: ForConfig::Stock {
+                    stock: "VTI".into(),
+                },
+                when_config: WhenConfig::GainAbove {
+                    gain_above_percent: 0.2,
+                },
+                do_config: DoConfig::SellAll { sell_all: true },
+            },
+        };
+        assert_eq!(expected, StrategyConfig::from(simple_config))
+    }
 }
